31d8cd7479ae4eda1b8efd02f2924363c9be2842
jason <793125943@qq.com>
2016-09-02 12:29:52 +0800

    Add files via upload

---
+++ daoxu2.java
@@ -0,0 +1,18 @@
+package สýื้;
+
+public class daoxu2 {
+
+	public static void main(String []args){
+		
+		int []a=new int []{1,22,33,44,55,66,77,88,99,100};
+		for(int i=0;i<a.length;i++){
+			System.out.println(a[i]+",");
+		}
+		for(int i=a.length-1;i>=0;i--){
+			System.out.print(a[i]+" ,");
+		}
+				
+	}
+	
+	
+}
